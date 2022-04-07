a534b1df7ac9991428249e5c3a37dbece5c95505
kanan <kananismayilzada017@gmail.com>
2022-04-06 22:32:05 -0400

    Reversed String from Kanan

---
+++ src/GithubFirstdayPractice/Reverse.java
@@ -0,0 +1,17 @@
+package GithubFirstdayPractice;
+
+public class Reverse {
+    public static void main(String[] args) {
+        String name ="Java";
+        String reversed = "";
+        for (int i = name.length()-1; i >= 0; i--) {
+            reversed += name.charAt(i);
+
+        }
+        System.out.println(reversed);
+    }
+
+
+
+
+}
