5b43f10380a6482afdfe04bf700231254916726f
vidosava111 <vidosavap1001@gmail.com>
2022-06-27 21:56:44 -0400

    mycode

--- src/practice01/PracticeGit.java
+++ src/practice01/PracticeGit.java
@@ -4,6 +4,14 @@ public class PracticeGit {
     public static void main(String[] args) {
 
         System.out.println("Hello everyone I hope to find you well through this massage");
+        // I love my group :)
+
+        //We are the best
+
+        //I'm sure
+
+
+
     }
 
 
