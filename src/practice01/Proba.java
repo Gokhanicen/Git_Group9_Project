9bef733b20705ff69c6b3b1b8f918111ec4c7d3c
vidosava111 <vidosavap1001@gmail.com>
2022-06-29 00:37:48 -0400

    proba with token

--- src/practice01/Proba.java
+++ src/practice01/Proba.java
@@ -3,5 +3,7 @@ package practice01;
 public class Proba {
     public static void main(String[] args) {
         System.out.println("tryout");
+
+        System.out.println("I got a token");
     }
 }
